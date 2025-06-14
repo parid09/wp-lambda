ea6355db1f50e7dfd44ffea4ed74e23ac773dfd3
parid09 <aishak007@hotmail.com>
2025-06-14 10:55:42 +0530

    Update serverless-functions.php
    updated actual wordpress url

--- serverless-functions.php
+++ serverless-functions.php
@@ -7,7 +7,7 @@ require( dirname( __FILE__ ) . '/vendor/autoload.php' );
 function serv_wptexturize($text){
 	$client = new GuzzleHttp\Client();
 	$totexturize = urlencode($text);
-	$response = $client->get('https://v2y230wfv1.execute-api.us-east-1.amazonaws.com/Prod?totexturize=' . $totexturize, ['verify' => false]);
+	$response = $client->get('https://aet7i4bu4h.execute-api.us-east-1.amazonaws.com/Prod/?totexturize=' . $totexturize, ['verify' => false]);
 	$texturized = json_decode($response->getBody(), true);
 	return $texturized['texturized'];
 }
